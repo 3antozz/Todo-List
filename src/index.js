200c174629446d29c3de1a6e46446ecf937ab11e
3antozz <106344848+3antozz@users.noreply.github.com>
2024-10-14 16:57:07 +0100

    add icons to navbar and project name header

--- src/index.js
+++ src/index.js
@@ -611,7 +611,7 @@ function sortProjectsButtonsHandler (projectsHandler, clickHandler, sortDates) {
             const tasks = sortDates.getUpcomingTodos();
             displaySortedTasks(tasks);
             if (!(projectsHandler.getAllProjects().length === 0)) {
-                setProjectHeaderName("Upcoming");
+                setProjectHeaderName(`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>calendar-month-outline</title><path d="M7 11H9V13H7V11M21 5V19C21 20.11 20.11 21 19 21H5C3.89 21 3 20.1 3 19V5C3 3.9 3.9 3 5 3H6V1H8V3H16V1H18V3H19C20.11 3 21 3.9 21 5M5 7H19V5H5V7M19 19V9H5V19H19M15 13V11H17V13H15M11 13V11H13V13H11M7 15H9V17H7V15M15 17V15H17V17H15M11 17V15H13V17H11Z" /></svg> Upcoming`);
             }
         })
     })();
@@ -623,7 +623,7 @@ function sortProjectsButtonsHandler (projectsHandler, clickHandler, sortDates) {
             const todayTasks = sortDates.getTodayTodos();
             displaySortedTasks(todayTasks);
             if (!(projectsHandler.getAllProjects().length === 0)) {
-                setProjectHeaderName("Today");
+                setProjectHeaderName(`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>timer-sand-complete</title><path d="M18 22H6V16L10 12L6 8V2H18V8L14 12L18 16M8 7.5L12 11.5L16 7.5V4H8M12 12.5L8 16.5V20H16V16.5M14 18H10V17.2L12 15.2L14 17.2Z" /></svg> Today`);
             }
         })
     })();
@@ -635,7 +635,7 @@ function sortProjectsButtonsHandler (projectsHandler, clickHandler, sortDates) {
             const tomorrowTasks = sortDates.getTomorrowTodos();
             displaySortedTasks(tomorrowTasks);
             if (!(projectsHandler.getAllProjects().length === 0)) {
-                setProjectHeaderName("Tomorrow");
+                setProjectHeaderName(`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>weather-night</title><path d="M17.75,4.09L15.22,6.03L16.13,9.09L13.5,7.28L10.87,9.09L11.78,6.03L9.25,4.09L12.44,4L13.5,1L14.56,4L17.75,4.09M21.25,11L19.61,12.25L20.2,14.23L18.5,13.06L16.8,14.23L17.39,12.25L15.75,11L17.81,10.95L18.5,9L19.19,10.95L21.25,11M18.97,15.95C19.8,15.87 20.69,17.05 20.16,17.8C19.84,18.25 19.5,18.67 19.08,19.07C15.17,23 8.84,23 4.94,19.07C1.03,15.17 1.03,8.83 4.94,4.93C5.34,4.53 5.76,4.17 6.21,3.85C6.96,3.32 8.14,4.21 8.06,5.04C7.79,7.9 8.75,10.87 10.95,13.06C13.14,15.26 16.1,16.22 18.97,15.95M17.33,17.97C14.5,17.81 11.7,16.64 9.53,14.5C7.36,12.31 6.2,9.5 6.04,6.68C3.23,9.82 3.34,14.64 6.35,17.66C9.37,20.67 14.19,20.78 17.33,17.97Z" /></svg> Tomorrow`);
             }
         })
     })();
@@ -647,7 +647,7 @@ function sortProjectsButtonsHandler (projectsHandler, clickHandler, sortDates) {
             const WeekTasks = sortDates.getWeekTodos();
             displaySortedTasks(WeekTasks);
             if (!(projectsHandler.getAllProjects().length === 0)) {
-                setProjectHeaderName("This Week");
+                setProjectHeaderName(`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>calendar-week-outline</title><path d="M5 3H6V1H8V3H16V1H18V3H19C20.11 3 21 3.89 21 5V19C21 20.1 20.11 21 19 21H5C3.9 21 3 20.11 3 19V5C3 3.89 3.9 3 5 3M5 19H19V9H5V19M5 7H19V5H5V7M17 11V13H7V11H17" /></svg> This Week`);
             }
         })
     })();
@@ -659,7 +659,7 @@ function sortProjectsButtonsHandler (projectsHandler, clickHandler, sortDates) {
             const CompleteTasks = projectsHandler.getAllCompletedTasks();
             displaySortedTasks(CompleteTasks);
             if (!(projectsHandler.getAllProjects().length === 0)) {
-                setProjectHeaderName("Completed Tasks");
+                setProjectHeaderName(`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>clipboard-check-outline</title><path d="M19,3H14.82C14.4,1.84 13.3,1 12,1C10.7,1 9.6,1.84 9.18,3H5A2,2 0 0,0 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5A2,2 0 0,0 19,3M12,3A1,1 0 0,1 13,4A1,1 0 0,1 12,5A1,1 0 0,1 11,4A1,1 0 0,1 12,3M7,7H17V5H19V19H5V5H7V7M7.5,13.5L9,12L11,14L15.5,9.5L17,11L11,17L7.5,13.5Z" /></svg> Completed Tasks`);
             }
         })
     })();
@@ -671,7 +671,7 @@ function sortProjectsButtonsHandler (projectsHandler, clickHandler, sortDates) {
             const importantTasks = projectsHandler.getAllImportantTasks();
             displaySortedTasks(importantTasks );
             if (!(projectsHandler.getAllProjects().length === 0)) {
-                setProjectHeaderName("Important Tasks");
+                setProjectHeaderName(`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>star-outline</title><path d="M12,15.39L8.24,17.66L9.23,13.38L5.91,10.5L10.29,10.13L12,6.09L13.71,10.13L18.09,10.5L14.77,13.38L15.76,17.66M22,9.24L14.81,8.63L12,2L9.19,8.63L2,9.24L7.45,13.97L5.82,21L12,17.27L18.18,21L16.54,13.97L22,9.24Z" /></svg> Important Tasks`);
             }
         })
     })();
