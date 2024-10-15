aed1e8a1b1931beb9978c949d333e18aea03a9f5
3antozz <106344848+3antozz@users.noreply.github.com>
2024-10-15 21:35:56 +0100

    Finish overall styling

--- src/DOMHandler.js
+++ src/DOMHandler.js
@@ -11,18 +11,18 @@ const displayNavProject = function (project) {
 
     const header = document.createElement("button");
     header.classList.add("project-nav")
-    header.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>pound</title><path d="M5.41,21L6.12,17H2.12L2.47,15H6.47L7.53,9H3.53L3.88,7H7.88L8.59,3H10.59L9.88,7H15.88L16.59,3H18.59L17.88,7H21.88L21.53,9H17.53L16.47,15H20.47L20.12,17H16.12L15.41,21H13.41L14.12,17H8.12L7.41,21H5.41M9.53,9L8.47,15H14.47L15.53,9H9.53Z" /></svg>${project.name}`;
+    header.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>Project</title><path d="M5.41,21L6.12,17H2.12L2.47,15H6.47L7.53,9H3.53L3.88,7H7.88L8.59,3H10.59L9.88,7H15.88L16.59,3H18.59L17.88,7H21.88L21.53,9H17.53L16.47,15H20.47L20.12,17H16.12L15.41,21H13.41L14.12,17H8.12L7.41,21H5.41M9.53,9L8.47,15H14.47L15.53,9H9.53Z" /></svg>${project.name}`;
     header.style.fill = colors[colorIndex];
     header.dataset.index = project.index;
 
     const editProject = document.createElement("button");
     editProject.classList.add("project-edit-button");
-    editProject.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>pencil</title><path d="M20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18,2.9 17.35,2.9 16.96,3.29L15.12,5.12L18.87,8.87M3,17.25V21H6.75L17.81,9.93L14.06,6.18L3,17.25Z" /></svg>`;
+    editProject.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>Edit</title><path d="M20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18,2.9 17.35,2.9 16.96,3.29L15.12,5.12L18.87,8.87M3,17.25V21H6.75L17.81,9.93L14.06,6.18L3,17.25Z" /></svg>`;
     editProject.dataset.index = project.index;
 
     const removeProject = document.createElement("button");
     removeProject.classList.add("project-remove-button");
-    removeProject.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>delete</title><path d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z" /></svg>`;
+    removeProject.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>Remove</title><path d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z" /></svg>`;
     removeProject.dataset.index = project.index;
 
     projectNavDiv.append(header, editProject, removeProject);
@@ -40,7 +40,7 @@ const displayAllProjectsNav = function (projects) {
 
 const displayProjectName = function (project) {
     const projectName = document.querySelector(".project-name")
-    projectName.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>pound</title><path d="M5.41,21L6.12,17H2.12L2.47,15H6.47L7.53,9H3.53L3.88,7H7.88L8.59,3H10.59L9.88,7H15.88L16.59,3H18.59L17.88,7H21.88L21.53,9H17.53L16.47,15H20.47L20.12,17H16.12L15.41,21H13.41L14.12,17H8.12L7.41,21H5.41M9.53,9L8.47,15H14.47L15.53,9H9.53Z" /></svg> ${project.name}`;
+    projectName.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>Project</title><path d="M5.41,21L6.12,17H2.12L2.47,15H6.47L7.53,9H3.53L3.88,7H7.88L8.59,3H10.59L9.88,7H15.88L16.59,3H18.59L17.88,7H21.88L21.53,9H17.53L16.47,15H20.47L20.12,17H16.12L15.41,21H13.41L14.12,17H8.12L7.41,21H5.41M9.53,9L8.47,15H14.47L15.53,9H9.53Z" /></svg> ${project.name}`;
 }
 
 const setProjectHeaderName = function (name) {
@@ -108,25 +108,25 @@ const displayTodo = function (project, todo, index, addRightDiv = true ) {
 
     const expandButton = document.createElement("button");
     expandButton.classList.add("expand");
-    expandButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>triangle-down</title><path d="M1 3H23L12 22" /></svg>`;
+    expandButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>Expand</title><path d="M1 3H23L12 22" /></svg>`;
     expandButton.dataset.index = index;
     expandButton.dataset.projectIndex = todo.projectIndex;
 
     const editButton = document.createElement("button");
     editButton.classList.add("edit");
-    editButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>pencil</title><path d="M20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18,2.9 17.35,2.9 16.96,3.29L15.12,5.12L18.87,8.87M3,17.25V21H6.75L17.81,9.93L14.06,6.18L3,17.25Z" /></svg>`;
+    editButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>Edit</title><path d="M20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18,2.9 17.35,2.9 16.96,3.29L15.12,5.12L18.87,8.87M3,17.25V21H6.75L17.81,9.93L14.06,6.18L3,17.25Z" /></svg>`;
     editButton.dataset.index = index;
     editButton.dataset.projectIndex = todo.projectIndex;
 
     const removeButton = document.createElement("button");
     removeButton.classList.add("task-remove");
-    removeButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>delete</title><path d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z" /></svg>`;
+    removeButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>Remove</title><path d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z" /></svg>`;
     removeButton.dataset.index = index;
     removeButton.dataset.projectIndex = todo.projectIndex;
 
     const important = document.createElement("button");
     important.classList.add("important-button");
-    important.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>star-outline</title><path d="M12,15.39L8.24,17.66L9.23,13.38L5.91,10.5L10.29,10.13L12,6.09L13.71,10.13L18.09,10.5L14.77,13.38L15.76,17.66M22,9.24L14.81,8.63L12,2L9.19,8.63L2,9.24L7.45,13.97L5.82,21L12,17.27L18.18,21L16.54,13.97L22,9.24Z" /></svg>`;
+    important.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>Important</title><path d="M12,15.39L8.24,17.66L9.23,13.38L5.91,10.5L10.29,10.13L12,6.09L13.71,10.13L18.09,10.5L14.77,13.38L15.76,17.66M22,9.24L14.81,8.63L12,2L9.19,8.63L2,9.24L7.45,13.97L5.82,21L12,17.27L18.18,21L16.54,13.97L22,9.24Z" /></svg>`;
     important.dataset.index = index;
     important.dataset.projectIndex = todo.projectIndex;
 
@@ -157,7 +157,7 @@ const displayTodo = function (project, todo, index, addRightDiv = true ) {
     rightDiv.classList.add("right");
 
     const priorityPara = document.createElement("p");
-    priorityPara.textContent = `#${project.name}`;
+    priorityPara.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>Project</title><path d="M5.41,21L6.12,17H2.12L2.47,15H6.47L7.53,9H3.53L3.88,7H7.88L8.59,3H10.59L9.88,7H15.88L16.59,3H18.59L17.88,7H21.88L21.53,9H17.53L16.47,15H20.47L20.12,17H16.12L15.41,21H13.41L14.12,17H8.12L7.41,21H5.41M9.53,9L8.47,15H14.47L15.53,9H9.53Z" /></svg>${project.name}`;
 
     if (!addRightDiv) {
         priorityPara.style.display = "none";
@@ -217,7 +217,7 @@ const expandTodo = function (todoDiv, project, todoIndex, button) {
     detailDiv.append(priorityPara, detailPara);
     todoDiv.appendChild(detailDiv);
 
-    button.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>triangle</title><path d="M1,21H23L12,2" /></svg>`;
+    button.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>Hide</title><path d="M1,21H23L12,2" /></svg>`;
     button.classList.add("important-task");
 
     todoDiv.classList.add("expanded");
@@ -227,7 +227,7 @@ const retractTodo = function (todoDiv, button) {
     const detail = todoDiv.querySelector(".detail");
     detail.remove();
 
-    button.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>triangle-down</title><path d="M1 3H23L12 22" /></svg>`;
+    button.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>Expand</title><path d="M1 3H23L12 22" /></svg>`;
     button.classList.remove("important-task");
     todoDiv.classList.remove("expanded");
 }
@@ -269,7 +269,7 @@ const createSortButton = function () {
     const topDiv = document.querySelector(".top-nav");
     const button = document.createElement("button");
     button.classList.add("sort-button", "default");
-    button.textContent = "Sorted by Default";
+    button.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>sort</title><path d="M18 21L14 17H17V7H14L18 3L22 7H19V17H22M2 19V17H12V19M2 13V11H9V13M2 7V5H6V7H2Z" /></svg> Sorted by Default`;
 
     topDiv.appendChild(button);
 }
@@ -278,13 +278,13 @@ const switchSortButton = function () {
     const button = document.querySelector(".sort-button");
 
     if (button.classList.contains("ascending")) {
-        button.textContent = "Sorted by Priority";
+        button.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>sort</title><path d="M18 21L14 17H17V7H14L18 3L22 7H19V17H22M2 19V17H12V19M2 13V11H9V13M2 7V5H6V7H2Z" /></svg> Sorted by Priority`;
         button.classList.add("sort-priority");
         button.classList.remove("ascending");
     }
 
     else if (button.classList.contains("sort-priority")) {
-        button.textContent = "Sorted by Default";
+        button.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>sort</title><path d="M18 21L14 17H17V7H14L18 3L22 7H19V17H22M2 19V17H12V19M2 13V11H9V13M2 7V5H6V7H2Z" /></svg> Sorted by Default`;
         button.classList.remove("sort-priority");
         button.classList.add("default");
 
@@ -292,7 +292,7 @@ const switchSortButton = function () {
 
     else if (button.classList.contains("default")) {
 
-        button.textContent = "Sorted by Date (Ascending)";
+        button.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>sort</title><path d="M18 21L14 17H17V7H14L18 3L22 7H19V17H22M2 19V17H12V19M2 13V11H9V13M2 7V5H6V7H2Z" /></svg> Sorted by Date (Ascending)`;
         button.classList.add("ascending");
         button.classList.remove("default");
     }
